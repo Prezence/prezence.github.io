2ddf44428915e34ec4e3cc6956ccf4bf99521753
Kenneth Reilly <kenneth.reilly@hotmail.com>
2019-01-10 19:15:13 -0800

    Background, font, and colors

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prezence Website
+
+## Overview
+This repository contains the Prezence website source code, which is also hosted on GitHub Pages at [prezence.github.io](https://prezence.github.io)
+
---
+++ prezence.js
@@ -0,0 +1,31 @@
+class Prezence {
+
+    /**
+     * Initialize the website
+     */
+    static init() {
+
+        window.onscroll = Prezence.on_scroll
+        window.onresize = Prezence.on_resize
+    }
+
+    /**
+     * 
+     * @param { MouseEvent } ev 
+     */
+    static on_resize(ev) {
+
+        debugger
+    }
+
+    /**
+     * 
+     * @param { MouseEvent } ev 
+     */
+    static on_scroll(ev) {
+
+        debugger
+    }
+}
+
+Prezence.init()
